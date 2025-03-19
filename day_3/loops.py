86ee712aebd3e3c4ede1868ebc73c1a3016b22bf
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-19 17:05:42 +0530

    loops, for loop and nested for loop

---
+++ day_3/loops.py
@@ -0,0 +1,99 @@
+##conditinal statements 
+# if 
+# else
+# elif 
+
+# match 
+
+
+#loops
+#doing a same task again and again 
+
+# types of loop
+# for -- when we know the exact number of times the task is to be done
+# while -- when we dont know the no of times but we know a condition 
+
+
+# range(s,s,s) 
+# start 
+# stop 
+# step
+
+
+
+
+# rep=50
+# for i  in range(1,51,1):  ##1 2 3 ........49 50
+#     print(i,"python")
+
+# for num in range(100):# start:0   step:1
+#     print(num)
+
+
+# for num in range(100,0,-5):  #100
+#     print(num)
+
+
+
+# for num in range(1,101,1):  #1
+#     if num%2!=0:
+#         print(num)
+#         print("hi")
+
+
+# prime  1 and itself
+
+# num=int( input("Enter the number: ") )
+# factors=0
+# for i in range(1,num+1):
+#     if num%i==0:
+#         factors=factors+1
+# if factors==2:
+#     print("prime")
+# else:
+#     print("not prime")
+
+# * * * * *
+# * * * * * 
+# * * * * *
+# * * * * *
+# * * * * *
+# rows=5 
+
+# for i in range(1,rows+1): #1   -  5
+#     s=""
+#     for j in range(1,rows+1): #1 -5 
+#         s=s+"*"+" "   #* * * * * 
+#     print(s)
+
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+# s=""
+# for i in range(1,6):#1 2 3 4 5
+#     s+="* "
+#     print(s)  ## * * * * *
+
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
