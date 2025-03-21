f8f9fbafb3fd8f93c73cabb41ee8cebc063a11c2
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-21 17:10:37 +0530

    functions and types of functions

---
+++ day_5/function.py
@@ -0,0 +1,92 @@
+#while loop
+#control statements 
+#break , continue    and pass
+
+#functions  
+
+# n1=5 
+# n2=10
+# def name(a,b):
+#     print(a+b)
+
+# name(n1,n2)
+
+
+
+#lambda function 
+# fun= lambda : print("hello")
+# fun()
+
+# add=lambda x,y: x+y
+# print(add(5,3))
+
+# pow=lambda a : a**a
+# print(pow(2))
+
+
+# power=lambda b,p,b2,p2: (b**p ,b2**p2)
+# b=input("enter the base: ")
+# p=input("enter the power: ")
+# print(power(int(b),int(p),6,7))
+
+
+
+
+# def sum(a,b=0,):
+#     return a+b
+# print(sum(5,-8))
+
+
+# def sub(a,b,c=0,):
+#     return a-b
+# print(sub(b=9,a=8))
+
+# def sum(a,b,/): ##positional only arguments 
+#     return a+b
+# print(sum(10,8))
+
+## *args,  --list
+## **kwargs  --dict 
+
+# named fun 
+# lambda fun
+#hof
+
+#sum of two  
+#check if the val is even
+
+
+# def is_even(n):  #6
+#     return n%2==0  #True
+
+
+# def sum(a,b,is_even):
+#     return is_even(a+b) ##1+5 6
+
+
+
+# print(sum(1,5,is_even))
+
+
+# args
+# positional 
+# default 
+# keyword 
+# positional only  (a,b,/)
+
+
+
+
+
+# datatype:
+# number  int float complex
+# collection  dict
+# boolean  True / False
+# sequence  tuple list str
+# set {}
+
+
+# user=input("enter the numebrs: ")
+# print(type(user))
+# print(len(user))
+# print(int(user))
