908e05c21d2c8f22ad189533a231c1595d31ceb3
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-03-27 20:16:24 +0530

    string datatype and methods

---
+++ day_9/strings.py
@@ -0,0 +1,102 @@
+# string:
+# it is a collection of characters enclosed in '' or "" or  ''''''  or """"""
+#it is a single value 
+#it is immutable 
+#we can access string using indexes
+
+
+
+# print(name[0])
+# print(len(name))
+# print(name[len(name)-1])
+# name[0]="C"
+
+# name="chandu is learning python"
+# for i in range(len(name)):
+#     print(name[i])
+
+# print("C"=="C")
+
+#len 
+# name="'ram' prasad"
+#'''''' """"""
+# story="hare ram is learning python" 
+# "chandu is listening"
+
+# story='''
+# hare ram is learning python 
+
+# chandu is not listening class
+
+# '''
+
+# print(story)
+
+
+# word="pythON"
+#upper()
+# print(word.upper())
+
+#lower()
+# print(word.lower())
+
+#swapcase()
+# print(word.swapcase())
+
+
+
+
+# word="      hello world              " 
+
+#strip
+# print(word.strip())
+
+
+#lstrip 
+# print(word.lstrip()) 
+
+
+#rstrip
+# print(word.rstrip())
+
+
+# sentence="this is the python class kkkkkkkkkkkkkkkkkkkk"
+
+#replace ()
+
+# print(sentence.replace("k","this is the extra string in place of k"))
+
+
+# word="python"
+#startswith
+# print(word.startswith("P")) 
+
+#endswith
+# print(word.endswith("o"))
+
+
+# username="manivardhan"
+#isalpha 
+# print(username.isalpha())
+
+# mob="9848022338"
+#isdigit
+# print(mob.isdigit())
+
+
+# val="123" 
+#isalnum
+# print(val.isalnum())
+
+# name="hello mr.manivardhan"
+# #find
+# print(name.find("an"))
+
+
+# val="hello"
+#zfill -- zero fill 
+
+# print(val.zfill(5))
+
+#center 
+# print(val.center(15,"-"))
---
+++ day_9/tuple.py
@@ -0,0 +1,46 @@
+# set 
+# it is a collection of unique values 
+# it is denoted with {1,2,3,}
+
+# add 
+# remove 
+# discard 
+# pop 
+# clear 
+
+
+
+# union 
+# intersection 
+# difference 
+# symmetric_difference()
+# copy 
+# x.issuperset(y)
+# x.issubset(y) 
+# issisjoint
+
+
+# list 
+# dict 
+# set 
+# tuple 
+
+# it is also collection datatype 
+# (1,2,3,54,5,6) 
+# it allows duplicate values 
+# it can be accessed using indexes 
+# it is immutable
+
+# tup[0]=10
+# print(tup[0])
+
+# print(len(tup))
+# for i in range(len(tup)):
+#     print(tup[i])
+
+tup=(True,[],{},1,2,3,5,6,1,2,3,"hello")
+#count
+# print(tup.count(10))
+#index
+# print(tup.index(1))
+
