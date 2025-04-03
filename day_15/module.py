f4ac9f97a1c00fb0764351368a2ec8d62ec8daea
Manivardhan-10k <manivardhan.10kcoders@gmail.com>
2025-04-03 17:06:00 +0530

    modules in python , math module

---
+++ day_15/module.py
@@ -0,0 +1,56 @@
+# Exception handling methods: 
+# try
+# except 
+# else:
+# finally
+
+
+# try:
+#     #block of code 
+#      age=20+""
+# except Exception as error:
+#     print({"error":error})
+# else:
+#      if age:
+#           print(age)
+# finally:
+#      print("always executed")     
+# print("hello")
+
+
+# print(f"the sum of 2,3 {10/0}")
+
+
+# Modules:
+
+# 1. built -in 
+# math , random ,time ,re ,os
+# 2. custom /userdefined 
+# .py
+# 3. third party modules 
+# pip install numpy pandas matplotlib
+
+
+import math
+
+# print(math.ceil(1.2))
+# val=math.floor(3.9)
+# print(val)
+# print(math.factorial(10))
+# print(math.pow(25,2))
+# print(math.sqrt(620))
+# print(math.pi)
+
+
+
+# num=int(input("enter the number: "))  #16
+# if math.sqrt(num) > math.floor(math.sqrt(num)):      #4.0 >4
+#     print("not perfect")
+# else:
+#     print("perfect square")
+# print(math.ceil(4.0))
+# print(math.floor(4.0))
+
+
+# import time 
+# print(time.time())
